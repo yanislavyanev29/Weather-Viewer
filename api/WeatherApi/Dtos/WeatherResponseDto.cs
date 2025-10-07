8c2111a3ad0e633c1171533ac8d286cf1b0b5fb9
yanislavyanev29 <yanislav00@gmail.com>
2025-10-07 23:10:37 +0300

    feat(api): add OpenWeather input models and public WeatherResponseDto

---
+++ api/WeatherApi/Dtos/WeatherResponseDto.cs
@@ -0,0 +1,17 @@
+
+// can be struct
+using System.Diagnostics.Eventing.Reader;
+/// <summary>
+/// Simplified DTO returned to the frontend application.
+/// Contains only the essential weather information.
+/// </summary>
+public  readonly  record struct WeatheResponseDto
+(
+    double Temperature,
+    string Description,
+    string Icon,
+    int Humidity,
+    double WindSpeed,
+    string Timezone
+
+);
